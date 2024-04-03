ecbfd340fae6323d818247d7beee98649c4a0e37
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-04-03 15:11:33 +0200

    Milestone 0

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script >
 import Main from './components/Main.vue'
 import Header from './components/Header.vue';
+import { store } from './components/data/store';
+import axios from 'axios';
 
 
 export default {
@@ -11,8 +13,22 @@ export default {
   },
   data(){
     return{
-      
+      store
     }
+  },
+  methods:{
+    getApi(){
+      
+      axios.get(this.store.apiUrlMovies, {
+        params:this.store.params
+      })
+      .then(res =>{
+        console.log(res.data)
+      })
+    }
+  },
+  mounted(){
+    this.getApi()
   }
 
 }
--- src/components/data/store.js
+++ src/components/data/store.js
@@ -2,6 +2,16 @@
 
 import { reactive } from 'vue'
 
-export const store = reactive({
+export const store= reactive({
+  apiUrlMovies: 'https://api.themoviedb.org/3/search/movie',
+  params:{
+    api_key:'32b08ab0028447c78aa0efa27c36fc47',
+    query:'matrix'
 
+
+
+  }
+
+
+  
 })
